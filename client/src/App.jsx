dc543b24a3519ae196ebe1f78f5b91c01d320b12
mustafiz8g <mustafizur8g@gmail.com>
2025-08-26 09:04:51 +0600

    setup signin with clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,19 @@ import { Route, Routes } from 'react-router'
 import Login from './pages/Login'
 import Feed from './pages/Feed'
 import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
 import Connections from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
+import { useUser }  from '@clerk/clerk-react'
+import Layout from './pages/Layout'
 function App() {
-
+  const { user } = useUser()
   return (
    <>
    <Routes>
-    <Route  path='/' element = {<Login />}>
+    <Route  path='/' element = {!user ? <Login /> : <Layout />}>
      <Route index element = {<Feed />} />
      <Route path='messages' element = {<Messages />} />
      <Route path='messages/:userId' element = {<ChatBox />} />
